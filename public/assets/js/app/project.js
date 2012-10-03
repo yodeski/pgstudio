d942124f621e5eb153440edc9eaeb32a95e66310
libra guest <yodeski@hotmail.com>
2012-10-02 22:42:50 -0400

    aa

---
+++ public/assets/js/app/project.js
@@ -0,0 +1,11 @@
+require(["core", "sandbox"], function() {
+    //This function is called when scripts/helper/util.js is loaded.
+	
+    require.ready(function() {
+		//This function will be called when all the dependencies
+		//listed above are loaded. Note that this function could
+		//be called before the page is loaded. This callback is optional.
+		console.log('ready');
+        require(["app/modules"]);
+    });
+});
